265f8feacda564b282ee4bdf727f6d2ff5650bc4
Adiazx14 <adrianvaldes1439@gmail.com>
2021-10-12 23:01:55 -0400

    static files

--- connect/connect/settings.py
+++ connect/connect/settings.py
@@ -119,9 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+import os
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = BASE_DIR
+MEDIA_URL = '/media/'
