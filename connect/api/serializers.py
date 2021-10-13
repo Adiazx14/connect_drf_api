8ffb6ba4d6cc8b2137fa22912851007c92f87401
Adrián Valdés <adrianvaldes1439@gmail.com>
2021-10-12 23:05:02 -0400

    Delete connect directory

--- connect/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import fields, serializers
-from .models import Region
-
-class RegionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Region
-        fields = "__all__"
