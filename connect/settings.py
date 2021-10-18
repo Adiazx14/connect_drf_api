b4199395183cc8741613c55302da3281d2b66301
Adiazx14 <adrianvaldes1439@gmail.com>
2021-10-17 23:50:25 -0400

    aws s3

--- connect/settings.py
+++ connect/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'storages',
     'api'
 ]
 
@@ -143,3 +144,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# S3
