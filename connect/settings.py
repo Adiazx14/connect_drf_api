2ccdb62a63ed5ba3bda98eabc0efaa9b77338002
Adiazx14 <adrianvaldes1439@gmail.com>
2021-10-18 19:07:41 -0400

    s3 setup

--- connect/settings.py
+++ connect/settings.py
@@ -146,3 +146,9 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # S3
+AWS_STORAGE_BUCKET_NAME = 'connect-api-bucket'
+AWS_S3_REGION_NAME = 'us-west-2'  # e.g. us-east-2
+AWS_ACCESS_KEY_ID = os.environ['BUCKET_KEY']
+AWS_SECRET_ACCESS_KEY = os.environ['BUCKET_ACCESS']
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_DEFAULT_ACL = None
