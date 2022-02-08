1c7992b23c6d7c978883e32086374113589cd6dc
Adiazx14 <adrianvaldes1439@gmail.com>
2022-02-08 01:31:35 -0500

    regular settings

--- api/management/commands/createsu.py
+++ api/management/commands/createsu.py
@@ -6,4 +6,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not User.objects.filter(username="adiazx14").exists():
-            User.objects.create_superuser("adiazx14", "book@connectravel.us", "14189114")
+            User.objects.create_superuser("username", "email@gmail.com", "pass")
